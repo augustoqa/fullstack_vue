ad8aff5f74c62dcfaebf2983e2049afa3bfa48cd
Cesar Acual <chechaacual@gmail.com>
2020-06-13 13:17:24 -0600

    Basic input using refs

---
+++ form_handling/03-basic-input/main.js
@@ -0,0 +1,27 @@
+// Vue allows us to use refs (references) to easily access a DOM 
+// element in a component.
+
+const InputForm = {
+    template: `
+        <div class="input-form">
+            <form @submit="submitForm" class="ui form">
+                <div class="field">
+                    <input ref="newItem" type="text" placeholder="Add an item!">
+                </div>
+                <button class="ui button">Submit</button>
+            </form>
+        </div>`,
+    methods: {
+        submitForm(evt) {
+            evt.preventDefault();
+            console.log(this.$refs.newItem.value);
+        }
+    }
+};
+
+new Vue({
+    el: '#app',
+    components: {
+        'input-form': InputForm
+    }
+});
