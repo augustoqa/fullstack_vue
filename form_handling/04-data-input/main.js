30b7604c081241d89b15694d7da27d3db42e8c54
Cesar Acual <chechaacual@gmail.com>
2020-08-09 10:54:22 -0600

    Form Validation

--- form_handling/04-data-input/main.js
+++ form_handling/04-data-input/main.js
@@ -8,10 +8,12 @@ const InputForm = {
                 <div class="field">
                     <label>New Item</label>
                     <input v-model="fields.newItem" type="text" placeholder="Add an item!">
+                    <span style="color: red">{{ fieldErrors.newItem }}</span>
                 </div>
                 <div class="field">
                     <label>Email</label>
                     <input v-model="fields.email" type="text" placeholder="What's your email?" />
+                    <span style="color: red">{{ fieldErrors.email }}</span>
                 </div>
                 <div class="field">
                     <label>Urgency</label>
@@ -21,11 +23,13 @@ const InputForm = {
                         <option>Moderate</option>
                         <option>Urgent</option>
                     </select>
+                    <span style="color: red">{{ fieldErrors.urgency }}</span>
                 </div>
                 <div class="field">
                     <div class="ui checkbox">
                         <input v-model="fields.termsAndConditions" type="checkbox" />
                         <label>I accept the terms and conditions</label>
+                        <span style="color: red">{{ fieldErrors.termsAndConditions }}</span>
                     </div>
                 </div>
                 <button class="ui button">Submit</button>
@@ -45,14 +49,46 @@ const InputForm = {
                 urgency: '',
                 termsAndConditions: false
             },
+            fieldErrors: {
+                newItem: undefined,
+                email: undefined,
+                urgency: undefined,
+                termsAndConditions: undefined
+            },
             items: []
         }
     },
     methods: {
         submitForm(evt) {
-            this.items.push(this.fields.newItem);
-            this.newItem = '';
             evt.preventDefault();
+
+            this.fieldErrors = this.validateForm(this.fields);
+            if (Object.keys(this.fieldErrors).length) return;
+
+            this.items.push(this.fields.newItem);
+            this.fields.newItem = '';
+            this.fields.email = '';
+            this.fields.urgency = '';
+            this.fields.termsAndConditions = false;
+        },
+        validateForm(fields) {
+            const errors = {};
+            if (!fields.newItem) errors.newItem = "New Item Required";
+            if (!fields.email) errors.email = "Email Required";
+            if (!fields.urgency) errors.urgency = "Urgency Required";
+            if (!fields.termsAndConditions) {
+                errors.termsAndConditions = "Terms and conditions have to be approved";
+            }
+
+            if (fields.email && !this.isEmail(fields.email)) {
+                errors.email = "Invalid Email";
+            }
+
+            return errors;
+        },
+        isEmail(email) {
+            const re = /\S+@\S+\.\S+/;
+            return re.test(email);
         }
     }
 };
