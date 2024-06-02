7ececf027f029300e6f4f5ac7fac1c547b8b2165
Cesar Quisquinay <augustoacual@gmail.com>
2024-06-01 22:43:44 -0600

    Initial template

--- weather-app/src/App.vue
+++ weather-app/src/App.vue
@@ -1,3 +1,76 @@
 <template>
-  <h1>App</h1>
+  <div id="app" class="flex-align has-text-centered">
+    <p class="app__date has-text-weight-bold">{{ date }}</p>
+
+    <weather-container></weather-container>
+
+    <div class="app__cities">
+      <a href="#" v-for="city in cities" :key="city.id">{{ city.name }}</a>
+    </div>
+  </div>
 </template>
+
+<script>
+import WeatherContainer from '@/components/WeatherContainer.vue'
+
+export default {
+  components: {
+    WeatherContainer
+  },
+  data() {
+    return {
+      cities: [
+        { id: 5128581, name: 'New York City, New York' },
+        { id: 3435910, name: 'Buenos Aires, Argentina' },
+        { id: 524901, name: 'Moscow, Russia' },
+        { id: 1850147, name: 'Tokyo, Japan' },
+        { id: 2147714, name: 'Sydney, Australia' },
+        { id: 2332453, name: 'Lagos, Nigeria' }
+      ]
+    }
+  },
+  computed: {
+    date() {
+      return new Date().toDateString()
+    }
+  }
+}
+</script>
+
+<style>
+html,
+body {
+  height: 100%;
+}
+
+#app {
+  height: 100%;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#app.flex-align {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.app__date {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+}
+
+.app__cities {
+  position: absolute;
+  bottom: 40px;
+}
+
+.app__cities a {
+  color: #42b983;
+  padding: 0 10px;
+}
+</style>
---
+++ weather-app/src/components/WeatherContainer.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <div class="weather container">
+      <img src="../../public/favicon.ico" alt="" class="weather__image" />
+      <h1 class="subtitle weather__city">Guatemala</h1>
+      <p class="weather__description">Nublado</p>
+      <p class="weather__temperature">30 grados</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.weather__image {
+  width: 150px;
+  margin: 0 auto;
+}
+
+.weather__city {
+  font-size: 2.5rem;
+}
+
+.weather__description {
+  font-size: 1.5rem;
+}
+
+.weather__temperature {
+  font-size: 2.5rem;
+}
+
+.loader {
+  border: 5px solid #f3f3f3;
+  border-radius: 50%;
+  border-top: 5px solid #53bf8e;
+  width: 120px;
+  height: 120px;
+  -webkit-animation: spin 2s linear infinite; /* Safari */
+  animation: spin 2s linear infinite;
+}
+
+/* Safari */
+@-webkit-keyframes spin {
+  0% {
+    -webkit-transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+  }
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
